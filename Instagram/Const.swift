84b6fa38ea1b812aec3c35aa396620b862016394
NTTC-kkuribara <kattun.kuribara@ntt.com>
2021-09-25 15:14:29 +0900

    Lesson8全て反映

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by ITMS NTTcom on 2021/09/18.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
